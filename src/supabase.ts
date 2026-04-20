05f4ba315f5066ea324c678d76f4d71d849c1dd8
Helen Roberts <helensroberts79@gmail.com>
2026-04-20 16:54:43 +0100

    ready to deploy

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://etyqdnkkuhilixmxotag.supabase.co'
-const supabaseKey = 'sb_publishable_ed_aQAb_cThrWn7wsQ6Qlg_cdN9t0zF'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
